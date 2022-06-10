86303b2ff5c59cd7a3d40439f0e488f8fdd94c78
vulpz <vulpcod3z@gmail.com>
2022-06-10 15:46:00 -0400

    Adding NewLinker bring-a-link functionality.

---
+++ NewLinker.js
@@ -0,0 +1,14 @@
+// Async delay helper function.
+const delay = async (msec) => new Promise((resolve) => setTimeout(resolve, msec));
+
+/******************************
+ * BRINGALINK
+ * - Allows users to provide
+ * - link from IMF to
+ * - directly vote on track.
+ * ****************************
+ * 
+*/
+module.exports.bringALink = async () => {
+
+};
